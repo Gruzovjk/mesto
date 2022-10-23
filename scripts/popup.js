82412716868d5c8598e0285f77995d726825c022
Land Lover <filimonovebay@gmail.com>
2022-10-23 04:50:28 +0400

    fix: исправить ошибки после ревью

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,20 +7,17 @@ let editProfile = document.querySelector("#editProfile");
 // кнопки закрытия и сейва. пока сделал для общего класса попапов
 let closeButton = document.querySelector(".popup__close-button");
 let saveButton = document.querySelector(".popup__save-button");
-
-// функция для открытия/закрытия попапа редактирования профиля
+// открытиt/закрытиt попапа редактирования профиля
 function showPopup() {
   editProfile.classList.toggle("popup_opened");
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
 }
-// слушатели кнопок открытия/закрытия попапа редактирования профиля
-profileEditButton.addEventListener("click", showPopup);
-closeButton.addEventListener("click", showPopup);
-
 // имя профиля && описание профиля
 let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__about");
 // форма попапа
-let formElement = document.querySelector(".popup__container");
+let formElement = document.querySelector(".popup__form");
 // инпуты
 let inputName = document.querySelector("#popup__input-name");
 let inputAbout = document.querySelector("#popup__input-about");
@@ -33,3 +30,6 @@ function formSubmitHandler(evt) {
 }
 // слушатель сохранения формы
 formElement.addEventListener("submit", formSubmitHandler);
+// слушатели кнопок открытия/закрытия попапа редактирования профиля
+profileEditButton.addEventListener("click", showPopup);
+closeButton.addEventListener("click", showPopup);
