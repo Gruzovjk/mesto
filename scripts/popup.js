1d583077fd444e319ca98044aa6ef17fc878ec4e
Land Lover <filimonovebay@gmail.com>
2022-10-29 22:27:18 +0400

    fix: исправить названия переменных и функций, подготовиться к добавлению нового кода

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,39 +1,50 @@
-// кнопка редкатирования профиля на странице
-const profileEditButton = document.querySelector(".profile__edit-button");
-// общий класс всех попапов
-const popup = document.querySelectorAll(".popup");
-// попап редактирования профиля
+// попапы
 const editProfile = document.querySelector("#editProfile");
-// кнопки закрытия и сейва. пока сделал для общего класса попапов
-const closePopupButton = document.querySelector(".popup__close-button");
-const savePopupButton = document.querySelector(".popup__save-button");
-// открыть попап редактирования профиля
-function showPopup() {
-  editProfile.classList.toggle("popup_opened");
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-}
-// закрыть попап редактирования профиля
-function closePopup() {
-  editProfile.classList.toggle("popup_opened");
-}
-// имя профиля && описание профиля
+
+// ЭЛЕМЕНТЫ СТРАНИЦЫ
+//имя профиля && описание профиля
 const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__about");
-// форма попапа
-const formElement = document.querySelector(".popup__form");
+// кнопки открытия попапов
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+// pop-up's buttons
+const closePopupButton = editProfile.querySelector(".popup__close-button");
+const savePopupButton = editProfile.querySelector(".popup__save-button");
+
+// ФУНКЦИИ
+// открыть попап
+function showPopupEditProfile() {
+  editProfile.classList.toggle("popup_opened");
+  inputProfileName.value = profileName.textContent;
+  inputProfileAbout.value = profileAbout.textContent;
+}
+
+// закрыть попап
+function closePopupEditProfile() {
+  editProfile.classList.toggle("popup_opened");
+}
+
+// формы попап
+const formEditProfile = editProfile.querySelector(".popup__form");
+
 // инпуты
-const inputName = document.querySelector("#popup__input-name");
-const inputAbout = document.querySelector("#popup__input-about");
-// обработчик отправки формы
+const inputProfileName = editProfile.querySelector("#popup__input-name");
+const inputProfileAbout = editProfile.querySelector("#popup__input-about");
+
+// ОБРАБОТЧИКИ
+// обработчики отправки формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileAbout.textContent = inputAbout.value;
-  closePopup();
+  profileName.textContent = inputProfileName.value;
+  profileAbout.textContent = inputProfileAbout.value;
+  closePopupEditProfile();
 }
-// слушатель сохранения формы
-formElement.addEventListener("submit", formSubmitHandler);
-// слушатели кнопок открытия/закрытия попапа редактирования профиля
-profileEditButton.addEventListener("click", showPopup);
-closePopupButton.addEventListener("click", closePopup);
+
+// СЛУШАТЕЛИ
+// слушатели сохранения формы
+formEditProfile.addEventListener("submit", formSubmitHandler);
+
+// слушатели кнопок
+profileEditButton.addEventListener("click", showPopupEditProfile);
+closePopupButton.addEventListener("click", closePopupEditProfile);
