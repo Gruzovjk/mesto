42461cd9ad8d0680b3ec740b7e4c58872d360ed1
Land Lover <filimonovebay@gmail.com>
2022-11-03 19:25:12 +0400

    refactor: зум картинки поместил в слушатель клика на картинку карточки внутрь рендера карточки, немного причесал код

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,73 +1,62 @@
-// попапы
+// DOM-elements
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
 const popupEditProfile = document.querySelector(".popup_type_profile");
 const popupAddCard = document.querySelector(".popup_type_card-add");
-const popupZoomImg = document.querySelector(".popup_type_img");
-
-// формы попап
+const popupImg = document.querySelector(".popup_type_img");
+const btnOpenEditProfile = document.querySelector(".profile__edit-button");
+const btnOpenAddCard = document.querySelector(".profile__add-button");
+const cardContainer = document.querySelector(".elements__list");
+const cardTemplate = document
+  .querySelector(".card__template")
+  .content.querySelector(".card");
+// popup's elements
 const formEditProfile = popupEditProfile.querySelector(".popup__form");
 const formAddCard = popupAddCard.querySelector(".popup__form");
-// инпуты
 const inputProfileName = popupEditProfile.querySelector(".popup__profile-name");
 const inputProfileAbout = popupEditProfile.querySelector(
   ".popup__profile-about"
 );
 const inputCardName = popupAddCard.querySelector(".popup__card-name");
 const inputCardLink = popupAddCard.querySelector(".popup__card-link");
-
-// кнопки открытия попапов
-const btnOpenEditProfile = document.querySelector(".profile__edit-button");
-const btnOpenAddCard = document.querySelector(".profile__add-button");
-
-// pop-up's buttons
 const btnCloseEditProfile = popupEditProfile.querySelector(
   ".popup__close-button"
 );
 const btnCloseAddCard = popupAddCard.querySelector(".popup__close-button");
-//profile
-const profileName = document.querySelector(".profile__name");
-const profileAbout = document.querySelector(".profile__about");
-//zoom
-
-// CARDS
-const cardContainer = document.querySelector(".elements__list");
-const cardTemplate = document
-  .querySelector(".card__template")
-  .content.querySelector(".card");
-
-// открыть попап
+const btnCloseImg = popupImg.querySelector(".popup__close-button");
+//fncs
 const openPopup = (element) => {
   element.classList.toggle("popup_opened");
 };
-
-// закрыть попап
 const closePopup = (element) => {
   element.classList.toggle("popup_opened");
 };
-
-const removeCard = (evt) => {
-  const target = evt.target;
-  const currentListItemEl = target.closest(".card");
-  currentListItemEl.remove();
-};
-const likeCard = (evt) => {
-  evt.target.classList.toggle("card__like-button_active");
-};
-
 const renderCard = (element) => {
   const cardElement = cardTemplate.cloneNode(true);
   const deleteButton = cardElement.querySelector(".card__remove-button");
-  deleteButton.addEventListener("click", removeCard);
+  deleteButton.addEventListener("click", (evt) => {
+    const target = evt.target;
+    const currentListItemEl = target.closest(".card");
+    currentListItemEl.remove();
+  });
   const likeButton = cardElement.querySelector(".card__like-button");
-  likeButton.addEventListener("click", likeCard);
+  likeButton.addEventListener("click", (evt) => {
+    evt.target.classList.toggle("card__like-button_active");
+  });
+  const imgOpen = cardElement.querySelector(".card__img");
+  imgOpen.addEventListener("click", () => {
+    popupImg.querySelector(".popup__img").src = imgOpen.src;
+    popupImg.querySelector(".popup__img").alt = imgOpen.alt;
+    popupImg.querySelector(".popup__img-caption").textContent = imgOpen.alt;
+    openPopup(popupImg);
+  });
   cardElement.querySelector(".card__name").textContent = element.name;
   cardElement.querySelector(".card__img").src = element.link;
   cardElement.querySelector(".card__img").alt = element.name;
   cardContainer.prepend(cardElement);
 };
-
 initialCards.forEach(renderCard);
-
-// Handlers
+// handlers
 const handleFormEditProfile = (evt) => {
   evt.preventDefault();
   profileName.textContent = inputProfileName.value;
@@ -83,12 +72,9 @@ const handleFormAddCard = (evt) => {
   renderCard(el);
   closePopup(popupAddCard);
 };
-
-// Listeners
-// _forms
+// listeners
 formEditProfile.addEventListener("submit", handleFormEditProfile);
 formAddCard.addEventListener("submit", handleFormAddCard);
-// _buttons
 btnOpenEditProfile.addEventListener("click", () => {
   inputProfileName.value = profileName.textContent;
   inputProfileAbout.value = profileAbout.textContent;
@@ -104,4 +90,7 @@ btnCloseEditProfile.addEventListener("click", () => {
 });
 btnCloseAddCard.addEventListener("click", () => {
   closePopup(popupAddCard);
+});
+btnCloseImg.addEventListener("click", () => {
+  closePopup(popupImg);
 });
