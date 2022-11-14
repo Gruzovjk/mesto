a70a2ae201412c0a464406081bb98fa2351fd26e
Land Lover <filimonovebay@gmail.com>
2022-11-14 12:11:13 +0400

    refactor: создал объект селекторов

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,13 +1,46 @@
+const settings = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  fieldSelector: ".popup__set",
+  submitButtonSelector: ".popup__save-button",
+  inputErrorClass: "popup__input_type_error",
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    const fieldsetList = Array.from(
+      formElement.querySelectorAll(settings.fieldSelector)
+    );
+    fieldsetList.forEach((fieldSet) => {
+      setEventListeners(fieldSet);
+    });
+  });
+};
+// пока работает
+// и сейчас работает
+
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.add("popup__input_type_error");
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(settings.inputErrorClass);
   errorElement.textContent = errorMessage;
 };
 
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.remove("popup__input_type_error");
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(settings.inputErrorClass);
   errorElement.textContent = "";
+};
+
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
 };
 
 const hasInvalidInput = (inputList) => {
@@ -24,17 +57,13 @@ const toggleButtonState = (inputList, buttonElement) => {
   }
 };
 
-const isValid = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
 const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-  const buttonElement = formElement.querySelector(".popup__save-button");
+  const inputList = Array.from(
+    formElement.querySelectorAll(settings.inputSelector)
+  );
+  const buttonElement = formElement.querySelector(
+    settings.submitButtonSelector
+  );
   toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", () => {
@@ -44,30 +73,4 @@ const setEventListeners = (formElement) => {
   });
 };
 
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(".popup__form"));
-  formList.forEach((formElement) => {
-    formElement.addEventListener("submit", function (evt) {
-      evt.preventDefault();
-    });
-    const fieldsetList = Array.from(
-      formElement.querySelectorAll(".popup__set")
-    );
-    fieldsetList.forEach((fieldSet) => {
-      setEventListeners(fieldSet);
-    });
-  });
-};
-
-// валидацию писал как в тренажере.
-// пока не понимаю, зачем enableValidation
-// принимать классы элементов.
-// пошел смотреть livecoding
-
-enableValidation({
-  // formSelector: ".popup__form",
-  // inputSelector: ".popup__input",
-  // fieldSelector: ".popup__set",
-  // submitButtonSelector: ".popup__save-button",
-  // inputErrorClass: "popup__input_type_error",
-});
+enableValidation();
