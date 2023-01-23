5e5af2cd292be55d09a885a6a63e497998d2aac9
Land Lover <filimonovebay@gmail.com>
2023-01-23 15:46:13 +0400

    refactor: первая попытка настройки вебпак

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import "./index.css";
 import Card from "../scripts/components/Card.js";
 import FormValidator from "../scripts/components/FormValidator.js";
 import Section from "../scripts/components/Section.js";
